3bc39b7006879d441a58fa00e5928f09bc445e4a
klishevich <busation@gmail.com>
2016-05-09 12:46:39 +0300

    vertical align catalog

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,27 @@
+// фиксирование ширины меню
 $(document).ready(function() {
     $('#menu2 > li').each(function(){
     var wid = $(this).width();
     $(this).css('width', wid+'px');
   });
+});
+
+// центровка подписей каталога по высоте
+$(document).ready(function() {
+	var maxdivheight = 0;
+    $('.homepage1-catalog > div').each(function(){
+    	var height = $(this).height();
+    	if (height > maxdivheight) {
+    		maxdivheight = height;
+    	}
+    	console.log('maxdivheight', maxdivheight);
+    });
+    $('.homepage1-catalog > div').each(function(){
+    	var height = $(this).height();
+    	$(this).css('height', maxdivheight+'px');
+    	if (height < maxdivheight){
+    		var padding = (maxdivheight - height)/2;
+    		$(this).css('padding-top', padding+'px');
+    	}
+    });
 });
